65875b8e77685c78362fab214e8b74cafe7607cd
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-05-31 15:34:17 -0500

    update documentation for ffi, don't use signed size

--- src/vectors.rs
+++ src/vectors.rs
@@ -41,17 +41,27 @@ fn calculate_normal(v1: &Vec3A, v2: &Vec3A, v3: &Vec3A) -> Vec3A {
 pub mod ffi {
     use super::*;
 
+    /// A C wrapper for [calculate_smooth_normals](crate::vectors::calculate_smooth_normals).
+    /// `positions` and `normals` must both have length `pos_nrm_length`.
+    /// `indices` and `indices_length` define the collection of vertex indices.
+    /// The function writes the resulting smooth normals to the first `pos_nrm_length` elements of `normals`.
+    ///
+    /// The memory layout of the `positions` and `normals` array should be like the following to ensure compatibility with the 16 byte alignment of the [Vec3A] type.
+    /// The fourth value of each vector is included only for alignment purposes and does not affect the computation.
+    /// This gives a required size of at least `pos_nrm_length * 16` bytes for both arrays.
+    ///
+    /// `x0 y0 z0 _ x1 y1 z1 _ x2 y2 z2 _ ...`
     #[no_mangle]
     pub extern "C" fn calculate_smooth_normals(
-        pos: *const glam::Vec3A,
-        nrm: *mut glam::Vec3A,
-        vec_len: i32,
+        positions: *const glam::Vec3A,
+        normals: *mut glam::Vec3A,
+        pos_nrm_length: u32,
         indices: *const i32,
-        index_len: i32,
+        indices_length: u32,
     ) {
-        let pos = unsafe { std::slice::from_raw_parts(pos, vec_len as usize) };
-        let nrm = unsafe { std::slice::from_raw_parts_mut(nrm, vec_len as usize) };
-        let indices = unsafe { std::slice::from_raw_parts(indices, index_len as usize) };
+        let pos = unsafe { std::slice::from_raw_parts(positions, pos_nrm_length as usize) };
+        let nrm = unsafe { std::slice::from_raw_parts_mut(normals, pos_nrm_length as usize) };
+        let indices = unsafe { std::slice::from_raw_parts(indices, indices_length as usize) };
 
         update_smooth_normals(pos, nrm, indices);
     }
@@ -137,9 +147,9 @@ mod tests {
         ffi::calculate_smooth_normals(
             pos.as_ptr(),
             nrm.as_mut_ptr(),
-            pos.len() as i32,
+            pos.len() as u32,
             indices.as_ptr(),
-            indices.len() as i32,
+            indices.len() as u32,
         );
         assert_eq!(nrm[0], Vec3A::ONE.normalize());
         assert_eq!(nrm[1], Vec3A::ONE.normalize());
