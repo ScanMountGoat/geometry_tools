664513681634e54d6dff74782ece01dd126408dd
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-06-15 22:24:20 -0500

    document safety for smooth normals ffi

--- src/vectors.rs
+++ src/vectors.rs
@@ -41,27 +41,31 @@ fn calculate_normal(v1: &Vec3A, v2: &Vec3A, v3: &Vec3A) -> Vec3A {
 pub mod ffi {
     use super::*;
 
-    /// A C wrapper for [calculate_smooth_normals](crate::vectors::calculate_smooth_normals).
-    /// `positions` and `normals` must both have length `pos_nrm_length`.
+    /// A wrapper for [calculate_smooth_normals](crate::vectors::calculate_smooth_normals).
     /// `indices` and `indices_length` define the collection of vertex indices.
     /// The function writes the resulting smooth normals to the first `pos_nrm_length` elements of `normals`.
     ///
-    /// The memory layout of the `positions` and `normals` array should be like the following to ensure compatibility with the 16 byte alignment of the [Vec3A] type.
+    /// # Safety
+    ///
+    /// `positions` and `normals` must both have length `pos_nrm_length`.
+    /// The memory layout of the `positions` and `normals` array should have the xyz values in the first three floats 
+    /// of each vector of four floats to ensure compatibility with the 16 byte alignment of the [Vec3A] type.
+    ///
+    /// Example: `x0 y0 z0 _ x1 y1 z1 _ x2 y2 z2 _ ...`
+    ///
     /// The fourth value of each vector is included only for alignment purposes and does not affect the computation.
     /// This gives a required size of at least `pos_nrm_length * 16` bytes for both arrays.
-    ///
-    /// `x0 y0 z0 _ x1 y1 z1 _ x2 y2 z2 _ ...`
     #[no_mangle]
-    pub extern "C" fn calculate_smooth_normals(
+    pub unsafe extern "C" fn calculate_smooth_normals(
         positions: *const glam::Vec3A,
         normals: *mut glam::Vec3A,
         pos_nrm_length: u32,
         indices: *const i32,
         indices_length: u32,
     ) {
-        let pos = unsafe { std::slice::from_raw_parts(positions, pos_nrm_length as usize) };
-        let nrm = unsafe { std::slice::from_raw_parts_mut(normals, pos_nrm_length as usize) };
-        let indices = unsafe { std::slice::from_raw_parts(indices, indices_length as usize) };
+        let pos = std::slice::from_raw_parts(positions, pos_nrm_length as usize);
+        let nrm = std::slice::from_raw_parts_mut(normals, pos_nrm_length as usize);
+        let indices = std::slice::from_raw_parts(indices, indices_length as usize);
 
         update_smooth_normals(pos, nrm, indices);
     }
