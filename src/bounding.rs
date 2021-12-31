8986d2a52b6d72fece81327253a8814965fcb090
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-12-31 17:36:05 -0600

    calculating bounding spheres from spheres, closes #3

--- src/bounding.rs
+++ src/bounding.rs
@@ -2,6 +2,9 @@
 
 use glam::Vec3A;
 
+// TODO: Use a special struct for the result?
+// ex: BoundingSphere::from_points
+
 /// Calculates a bounding sphere of the form `(center, radius)` that containts all the specified points.
 /// The returned result will contain all points but may be larger than the optimal solution.
 /// # Examples
@@ -39,7 +42,7 @@ pub fn calculate_bounding_sphere_from_points(points: &[Vec3A]) -> (Vec3A, f32) {
     // Use the simple approach of averaging the points as the center.
     let center: Vec3A = points.iter().sum::<Vec3A>() / points.len() as f32;
 
-    // Find the smallest radius that contains all points.
+    // Find the smallest radius that contains all points given a center.
     let mut radius_squared = 0f32;
     for length_squared in points.iter().map(|p| p.distance_squared(center)) {
         if length_squared > radius_squared {
@@ -48,6 +51,32 @@ pub fn calculate_bounding_sphere_from_points(points: &[Vec3A]) -> (Vec3A, f32) {
     }
 
     (center, radius_squared.sqrt())
+}
+
+pub fn calculate_bounding_sphere_from_spheres(spheres: &[(Vec3A, f32)]) -> (Vec3A, f32) {
+    if spheres.is_empty() {
+        return (Vec3A::ZERO, 0f32);
+    }
+
+    // Use the simple approach of averaging the points as the center.
+    let center: Vec3A = spheres.iter().map(|s| &s.0).sum::<Vec3A>() / spheres.len() as f32;
+
+    // Find the smallest radius that contains all spheres given a center.
+    // This is a simple extension of testing for sphere-sphere intersection.
+    // We want the distance between centers to not exceed the sum of the radii.
+    // We iteratively increase the radius for each sphere.
+    // TODO: Share code with above?
+    let mut radius = 0f32;
+    for length in spheres
+        .iter()
+        .map(|(center2, radius2)| center2.distance(center) + radius2)
+    {
+        if length > radius {
+            radius = length;
+        }
+    }
+
+    (center, radius)
 }
 
 /// Calculates an axis-aligned bounding box (abbreviated aabb) of the form `(min_xyz, max_xyz)` containing all the specified points.
@@ -94,13 +123,25 @@ pub fn calculate_aabb_from_points(points: &[Vec3A]) -> (Vec3A, Vec3A) {
 
 #[cfg(test)]
 mod tests {
-    use std::ops::Sub;
-
     use super::*;
 
     fn sphere_contains_points(points: &[Vec3A], sphere: &(Vec3A, f32)) -> bool {
+        let (center, radius) = sphere;
         for point in points {
-            if point.sub(sphere.0).length() > sphere.1 {
+            if point.distance(*center) > *radius {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn sphere_contains_spheres(spheres: &[(Vec3A, f32)], sphere: &(Vec3A, f32)) -> bool {
+        // Two spheres intersect if the distance between their centers
+        // is less than the sum of their radii.
+        let (center, radius) = sphere;
+        for (center2, radius2) in spheres {
+            if center.distance(*center2) > radius + radius2 {
                 return false;
             }
         }
@@ -190,5 +231,31 @@ mod tests {
         // Check that all the corners are contained in the sphere.
         let bounding_sphere = calculate_bounding_sphere_from_points(&points);
         assert!(sphere_contains_points(&points, &bounding_sphere));
+    }
+
+    #[test]
+    fn sphere_no_spheres() {
+        let bounding_sphere = calculate_bounding_sphere_from_spheres(&[]);
+        assert_eq!((Vec3A::ZERO, 0f32), bounding_sphere);
+    }
+
+    #[test]
+    fn sphere_single_sphere() {
+        let spheres = vec![(Vec3A::new(0.1, 0.2, 0.3), 1.5)];
+
+        let bounding_sphere = calculate_bounding_sphere_from_spheres(&spheres);
+        assert!(sphere_contains_spheres(&spheres, &bounding_sphere));
+    }
+
+    #[test]
+    fn sphere_multiple_spheres() {
+        let spheres = vec![
+            (Vec3A::new(0.1, 0.2, 0.3), 1.5),
+            (Vec3A::new(-1.0, 5.0, 2.5), 3.2),
+            (Vec3A::new(4.0, 5.0, 6.0), 10.5),
+        ];
+
+        let bounding_sphere = calculate_bounding_sphere_from_spheres(&spheres);
+        assert!(sphere_contains_spheres(&spheres, &bounding_sphere));
     }
 }
